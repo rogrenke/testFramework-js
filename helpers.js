03a6fda62ca325292334bf5a3fef5b5091248862
rogrenke <roland@rolandgrenke.com>
2017-06-21 15:25:21 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -3,15 +3,15 @@
     this.var;
   };
 
-  Assert.prototype.isTrue = function(assertionToCheck, testName) {
-      if (!assertionToCheck) {
+  Assert.prototype.isTrue = function(assertionToCheck, testName=arguments.callee.name) {
+    if (!assertionToCheck) {
         throw new Error ("Assertion failed:" + assertionToCheck + " is not truthy");
       } else {
         console.log(testName + " test passed!");
       }
     }
 
-  Assert.prototype.isFalse = function(assertionToCheck, testName) {
+  Assert.prototype.isFalse = function(assertionToCheck, testName=arguments.callee.name) {
       if (assertionToCheck) {
         throw new Error ("Assertion failed:" + assertionToCheck + " is not falsy");
       } else {
@@ -22,7 +22,7 @@
     function Page() {
     };
 
-    Page.prototype.hasContent = function(element, expectedValue, testName) {
+    Page.prototype.hasContent = function(element, expectedValue, testName=arguments.callee.name) {
       if (element != expectedValue) {
         throw new Error (element + " is not on the page");
       } else {
